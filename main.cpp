774fb71454f3059e149bd4ce73cb5f4611d94d77
brianchum <bchum@students.cs.ubc.ca>
2021-05-22 14:57:45 -0700

    second commit

--- main.cpp
+++ main.cpp
@@ -5,12 +5,15 @@
 
 using namespace std;
 
+void Player_round(Player player, Flags flags);
+
 int main () {
     Player player;
     string name;
     Flags flags;
     Comp comp;
-    int input_player_choose;
+    char input_next_round;
+    int input_player_choose, model;
 
      // introduction of the game  
     cout << "" << '\n';    
@@ -31,6 +34,7 @@ int main () {
     cout << "" << '\n';
     cout << "   *  My flag is going to be look like this |@" <<'\n';
     cout << "                                            |" << '\n';
+    cout << "  " << '\n';  
     cout << "----------------------------------------------------------------------------------------" << '\n';  
     cout << "                                     LET GET START IT                                " << '\n'; 
     cout << "" << '\n';
@@ -40,41 +44,86 @@ int main () {
     cout << "|     |     |     |     |     |     |     |     |     |     |     " << '\n';
     cout << "" << '\n';
 
-    
-    
-    for (int round = 1; flags.getFlag(10) == 0 ; round++ ) {
-        cout << "                       Round  " << round <<'\n'; 
-        cout << "" << '\n';
-        cout << "How many flags do you want to take in this round ? (1/2)" <<'\n';
-        cin >> input_player_choose; 
-
-        // if user input is neither 1 or 2 do it again
-        while (input_player_choose != 1 && input_player_choose != 2) {
+    do {
+        Flags flags;
+        cout << " Would you like to go first (1) or after (2) ? " << '\n'; 
+        cin >> model;
+        while (model != 1 && model != 2) {
             cout << "Please try again, put in either (1) or (2)"<<'\n';
-            cout << "How many flags do you want to take in this round ? (1/2)" <<'\n';
+            cout << " Would you like to go first (1) or after (2) ? " << '\n';
             cin >> input_player_choose; 
         }
-        player.setPlayerChoose(input_player_choose);
-        flags.update_player_flags(player.getPlayerChoose());
-        flags.print_flags(player.getPlayerCap());
-        if (flags.getFlag(10)!=0) break;
+        if (model == 1) {
+            for (int round = 1; flags.getFlag(10) == 0 ; round++ ) {
+                cout << "" << '\n';
+                cout << "                       Round  " << round <<'\n'; 
+                cout << "" << '\n';
+                cout << "How many flags do you want to take in this round ? (1/2)" <<'\n';
+                cin >> input_player_choose; 
 
-        cout << "" << '\n';
-        cout << "----------------------------------------------------------------------------------------" << '\n';  
-        cout << "Alright it is my turn" << '\n';
-        cout << "" << '\n';
-        cout << "I am going to take " << comp.setCompChoose(flags.get_temp_flags()) << " flags "<<'\n';  
-        cout << "" << '\n';
-        flags.update_comp_flags(comp.getCompChoose());
-        flags.print_flags(player.getPlayerCap());
-        if (flags.getFlag(10) != 0) break;
-         cout << "----------------------------------------------------------------------------------------" << '\n';  
-    }
+                // if user input is neither 1 or 2 do it again
+                while (input_player_choose != 1 && input_player_choose != 2) {
+                    cout << "Please try again, put in either (1) or (2)"<<'\n';
+                    cout << "How many flags do you want to take in this round ? (1/2)" <<'\n';
+                    cin >> input_player_choose; 
+                }
+                player.setPlayerChoose(input_player_choose);
+                flags.update_player_flags(player.getPlayerChoose());
+                flags.print_flags(player.getPlayerCap());
 
-    if (flags.getFlag(10) == 1) {
-       cout << "You win" << '\n'; 
-    } else if (flags.getFlag(10) == 2){
-       cout << "I told you, you can't win" << '\n'; 
-    }
-    return 0;
+                if (flags.getFlag(10)!=0) break;
+
+                cout << "" << '\n';
+                cout << "----------------------------------------------------------------------------------------" << '\n';  
+                cout << "Alright it is my turn" << '\n';
+                cout << "" << '\n';
+                cout << "I am going to take " << comp.setCompChoose(flags.get_temp_flags()) << " flags "<<'\n';  
+                cout << "" << '\n';
+                flags.update_comp_flags(comp.getCompChoose());
+                flags.print_flags(player.getPlayerCap());
+                if (flags.getFlag(10) != 0) break;
+                cout << "----------------------------------------------------------------------------------------" << '\n';  
+            }
+
+        } else {
+            for (int round = 1; flags.getFlag(10) == 0 ; round++ ) {
+
+                cout << "" << '\n';
+                cout << "                       Round  " << round <<'\n'; 
+                cout << "" << '\n';
+                cout << "I am going to take " << comp.setCompChoose(flags.get_temp_flags()) << " flags "<<'\n';  
+                cout << "" << '\n';
+                flags.update_comp_flags(comp.getCompChoose());
+                flags.print_flags(player.getPlayerCap());
+                if (flags.getFlag(10) != 0) break;
+                cout << "Alright it is your turn" << '\n';
+                cout << "How many flags do you want to take in this round ? (1/2)" <<'\n';
+                cin >> input_player_choose; 
+                while (input_player_choose != 1 && input_player_choose != 2) {
+                    cout << "Please try again, put in either (1) or (2)"<<'\n';
+                    cout << "How many flags do you want to take in this round ? (1/2)" <<'\n';
+                    cin >> input_player_choose; 
+                }
+                player.setPlayerChoose(input_player_choose);
+                flags.update_player_flags(player.getPlayerChoose());
+                flags.print_flags(player.getPlayerCap());
+                if (flags.getFlag(10)!=0) break;
+            }
+        }
+
+        if (flags.getFlag(10) == 1) {
+            cout << "You win" << '\n'; 
+        } else if (flags.getFlag(10) == 2){
+            cout << "I told you, you can't win" << '\n'; 
+        }
+
+        cout << "" << '\n'; 
+        cout << "Do you want to play and other round ? (y/n)" << '\n'; 
+        cin >> input_next_round;
+
+    } while(input_next_round == 'y');
+
+    cout << "" << '\n';
+    cout << "See you next time " << '\n'; 
+    cout << "" << '\n';
 }
